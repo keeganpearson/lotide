7214b2734256480d41620c1efc12cf040c135151
keeganpearson <keegan.dpearson@gmail.com>
2023-04-21 20:52:17 +0000

    Function that will take in an array and return the middle-most element(s) of the given array - but not if it only has 1 or 2 elements

---
+++ middle.js
@@ -0,0 +1,37 @@
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = function(array) {
+ let middleIndex = Math.floor(array.length / 2); 
+ if (array.length <= 2) {
+ return [];
+ } else if (array.length % 2 === 1) {
+  return [array[middleIndex]];
+ } else {
+  return [array[middleIndex - 1], array[middleIndex]];
+ }
+}; 
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
