fbb0dc64e6093569a22f526cf96b2d360799a2cd
keeganpearson <keegan.dpearson@gmail.com>
2023-04-21 20:35:13 +0000

    Function that flattens all nested arrays into one array

---
+++ flatten.js
@@ -0,0 +1,35 @@
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(arr) {
+  let flattenedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flattenedArr.push(arr[i][j]);
+      }
+    } else {
+      flattenedArr.push(arr[i]);
+    }
+  }
+  return flattenedArr;
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
