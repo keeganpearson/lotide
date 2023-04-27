8b46434846a9593cae6178ed39fb12ddff2205c5
keeganpearson <keegan.dpearson@gmail.com>
2023-04-27 23:28:58 +0000

    Use .map function for first time in three different ways on an array

---
+++ map.js
@@ -0,0 +1,44 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+// Test case: first character of each word
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+
+// Test case: length of each word
+const results2 = map(words, word => word.length);
+console.log(results2);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+
+// Test case: words to uppercase
+const results3 = map(words, word => word.toUpperCase());
+console.log(results3);
+assertArraysEqual(results3, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
