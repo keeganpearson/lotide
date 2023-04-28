2af212283ebb50a0be75c89120e6d9e079585ab2
keeganpearson <keegan.dpearson@gmail.com>
2023-04-28 17:09:04 +0000

    Update syntax and comments

--- map.js
+++ map.js
@@ -28,17 +28,15 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-// Test case: first character of each word
+// Test cases: 
 const results1 = map(words, word => word[0]);
 console.log(results1);
 assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
 
-// Test case: length of each word
 const results2 = map(words, word => word.length);
 console.log(results2);
 assertArraysEqual(results2, [6, 7, 2, 5, 3]);
 
-// Test case: words to uppercase
 const results3 = map(words, word => word.toUpperCase());
 console.log(results3);
 assertArraysEqual(results3, ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
