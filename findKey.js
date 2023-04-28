2e73a34407647eb6e7fb02a42eae5c192d09277d
keeganpearson <keegan.dpearson@gmail.com>
2023-04-28 17:44:14 +0000

    Function that takes in object and callback, return first the callback returns a truthy value otherwise undefined

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const findKey = (obj, callback) => {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+//test code:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// test cases:
+
+// Test cases
+const restaurants = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(restaurants, x => x.stars === 2), "noma");
+assertEqual(findKey(restaurants, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(restaurants, x => x.stars === 5), undefined);
