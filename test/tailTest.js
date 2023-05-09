f1f612777597687c44a9fea728e1ccd7e17b98b4
keeganpearson <keegan.dpearson@gmail.com>
2023-05-09 22:15:52 +0000

    Refractored to use mocha and chai tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,12 @@
-const assertEqual = require('../assertEqual');
+const chai = require('chai');
+const assert = chai.assert;
 const tail = require('../tail');
 
-let arr = [0, 2, 3];
-let expected = [2, 3];
-let actual = tail(arr);
-
-assertEqual(actual, expected);
-console.log(actual);
+describe("#tail", () => {
+  it("returns [2, 3] when input is [0, 2, 3]", () => {
+    let arr = [0, 2, 3];
+    let expected = [2, 3];
+    let actual = tail(arr);
+    assert.deepEqual(actual, expected);
+  });
+});
