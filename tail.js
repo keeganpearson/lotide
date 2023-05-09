cd28808b2887d3e420618391b62e3e7ef4d874b9
keeganpearson <keegan.dpearson@gmail.com>
2023-05-09 21:14:06 +0000

    Moved tests to test file

--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const tail = function(x) {
   return x.slice(1);
 };
 
-let arr = [0, 2, 3];
-let expected = [2, 3];
-let actual = tail(arr);
 
-assertEqual(actual, expected);
-console.log(actual);gi
+module.exports = tail;
