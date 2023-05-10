ce7f6e1fb663b7b3a61dd5988699a75e1f5c866a
keeganpearson <keegan.dpearson@gmail.com>
2023-05-10 19:09:57 +0000

    Commented out tests and/or moved tests to separate mocha and chai testing file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,46 +1,47 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const eqArrays = function(array1, array2) {
+//   if (array1.length !== array2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] !== array2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
+// const eqObjects = function(object1, object2) {
+//   const keys1 = Object.keys(object1);
+//   const keys2 = Object.keys(object2);
 
-  for (const key of keys1) {
-    const value1 = object1[key];
-    const value2 = object2[key];
+//   if (keys1.length !== keys2.length) {
+//     return false;
+//   }
 
-    if (Array.isArray(value1) && Array.isArray(value2)) {
-      if (!eqArrays(value1, value2)) {
-        return false;
-      }
-    } else if (value1 !== value2) {
-      return false;
-    }
-  }
+//   for (const key of keys1) {
+//     const value1 = object1[key];
+//     const value2 = object2[key];
 
-  return true;
-};
+//     if (Array.isArray(value1) && Array.isArray(value2)) {
+//       if (!eqArrays(value1, value2)) {
+//         return false;
+//       }
+//     } else if (value1 !== value2) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -51,16 +52,19 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-assertObjectsEqual(shirtObject, anotherShirtObject); // Should pass
+// const shirtObject = { color: "red", size: "medium" };
+// const anotherShirtObject = { size: "medium", color: "red" };
+// assertObjectsEqual(shirtObject, anotherShirtObject); // Should pass
 
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertObjectsEqual(shirtObject, longSleeveShirtObject); // Should fail
+// const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+// assertObjectsEqual(shirtObject, longSleeveShirtObject); // Should fail
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject); // Should pass
+// const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+// const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+// assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject); // Should pass
 
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertObjectsEqual(multiColorShirtObject, longSleeveMultiColorShirtObject); // Should fail
+// const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+// assertObjectsEqual(multiColorShirtObject, longSleeveMultiColorShirtObject); // Should fail
+
+
+module.exports = assertObjectsEqual;
