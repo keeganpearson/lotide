8a57331237ca11750b0172e6abacb126c69333d4
keeganpearson <keegan.dpearson@gmail.com>
2023-05-10 19:12:13 +0000

    Commented out tests and/or moved tests to separate mocha and chai testing file

--- flatten.js
+++ flatten.js
@@ -1,22 +1,22 @@
-function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+// function eqArrays(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+// const assertArraysEqual = function(arr1, arr2) {
+//   if (eqArrays(arr1, arr2)) {
+//     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+//   }
+// };
 
 const flatten = function(arr) {
   let flattenedArr = [];
@@ -32,4 +32,6 @@ const flatten = function(arr) {
   return flattenedArr;
 }
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
+module.exports = flatten;
