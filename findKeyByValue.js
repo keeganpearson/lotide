51211c261e466205de4106add3b2efe3e2e1d202
keeganpearson <keegan.dpearson@gmail.com>
2023-05-10 19:11:55 +0000

    Commented out tests and/or moved tests to separate mocha and chai testing file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const findKeyByValue = function(object, value) {
   for (let key in object) {
@@ -15,17 +15,19 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-// Test cases
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire", 
-  star_wars: "Andor"
-};
+// // Test cases
+// const bestTVShowsByGenre = { 
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire", 
+//   star_wars: "Andor"
+// };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Malcom in the Middle"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Andor"), "star_wars");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Malcom in the Middle"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Andor"), "star_wars");
+
+module.exports = findKeyByValue;
