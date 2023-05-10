b86373396eaff2e6313fc5b48f9ce780c2bcbff9
keeganpearson <keegan.dpearson@gmail.com>
2023-05-10 19:11:30 +0000

    Commented out tests and/or moved tests to separate mocha and chai testing file

--- findKey.js
+++ findKey.js
@@ -7,27 +7,29 @@ const findKey = (obj, callback) => {
   return undefined;
 };
 
-//test code:
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// //test code:
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 // test cases:
 
-// Test cases
-const restaurants = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
+// // Test cases
+// const restaurants = {
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// };
 
-assertEqual(findKey(restaurants, x => x.stars === 2), "noma");
-assertEqual(findKey(restaurants, x => x.stars === 3), "Akaleri");
-assertEqual(findKey(restaurants, x => x.stars === 5), undefined);
+// assertEqual(findKey(restaurants, x => x.stars === 2), "noma");
+// assertEqual(findKey(restaurants, x => x.stars === 3), "Akaleri");
+// assertEqual(findKey(restaurants, x => x.stars === 5), undefined);
+
+module.exports = findKey;
